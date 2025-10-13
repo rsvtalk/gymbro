a1030bb54af3509349c39db2f02314db93f93c67
rsvtalk <rsv.talk@gmail.com>
2025-10-14 02:11:40 +0300

    gymbro init commit

--- src/main/java/GymBro/Configuration/SecurityConfig.java
+++ src/main/java/GymBro/Configuration/SecurityConfig.java
@@ -13,7 +13,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
